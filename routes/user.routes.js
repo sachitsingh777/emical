2d2c1e05e99b75819bb8853391c61b3c5a525c40
unknown <sachitsinghbais777@gmail.com>
2023-06-11 13:23:01 +0530

    mock11

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -45,10 +45,41 @@ userRouter.post("/login",async(req,res)=>{
 })
 
 userRouter.get("/getprofile",logged,async(req,res)=>{
-    const {name,email}=req.body
-    let user={name,email}
-    res.send(user)
+    const {userID}=req.body
+    let user=await UserModel.findOne({_id:userID})
+try{
+    if(user){
+        res.status(200).send({name:user.name,email:user.email})
+    }else{
+        res.send({msg:"something went wrong"})
+    }
+}
+catch(error){
+    res.send({err:error.message})
+}
    
 })
-
+emiRouter.get("/emical",async(req,res)=>{
+    try{
+        const {loan,annual,tenure}=req.query
+        loan=+loan
+        annual=+annual
+        tenure=+tenure
+    const rate=(annual/100)/12
+    
+    const emi = loan * rate* (1 + rate)**tenure / ((1 + rate)**tenure - 1) 
+   
+   const interest_pay=emi*tenure-loan;
+   const total_payment=emi*tenure
+   let data={
+    emi:emi.toFixed(2),
+    interest_pay:interest_pay.toFixed(2),
+    total_payment:total_payment.toFixed(2)
+   }
+    
+    res.status(200).send(data)
+}catch(error){
+    res.status(400).send({"err":error.message})
+}
+})
 module.exports={userRouter}
